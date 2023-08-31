906239525170d0569f1fd53f93235f5bdb6bf318
Mads Kristian Willadsen <mads482t@mail.hansenberg.dk>
2023-08-31 12:51:55 +0200

    mmm yes yes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Book lebook = new Book();
+List<Chapters> chapters = new List<Chapters>();
+
+Chapter variabler =new Chapter();
+Chapter data = new Chapter(); 
+chapters.Add(data);
+chapters.Add(variabler);
+
+List<Page> variablepages= new List<Page>();
+Page variablerpage = new Page(MyNotes.VariablesPageOne);
+Page variablerpage2 = new Page(MyNotes.VariablesPageTwo);
+variablepages.Add(variablerpage);
+variablepages.Add(variablerpage2);
+
+lebook.Start;
