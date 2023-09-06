9c1afd6f432768fe53236c0e003d47a25beb1aea
Mads Kristian Willadsen <mads482t@mail.hansenberg.dk>
2023-09-06 12:10:49 +0200

    weewooo notes
    OOOOooooo

--- Program.cs
+++ Program.cs
@@ -3,7 +3,13 @@
 Book lebook = new Book("GUUUHHH", pages);
 
 Page variables = new Page("variables", MyNotes.VariablesPageOne);
+Page loops = new Page("loops", MyNotes.loopspage);
+Page classes = new Page("classes", MyNotes.classespage);
 pages.Add(variables);
+pages.Add(loops); 
+pages.Add(classes);
+
+
 
 
 lebook.Start();
