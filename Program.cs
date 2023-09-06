f388da73280aba9e7df28673a20de504cda95492
Mads Kristian Willadsen <mads482t@mail.hansenberg.dk>
2023-09-06 12:43:15 +0200

    aaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAh
    sort

--- Program.cs
+++ Program.cs
@@ -4,13 +4,15 @@ List<Page> pages = new List<Page>();
 
 Book lebook = new Book("GUUUHHH", pages);
 
-string variableText = File.ReadAllText(@"pages\variables.txt", Encoding.UTF8);
+string[] files = Directory.GetFiles("pages");
+
+string variableText = File.ReadAllText(files[2], Encoding.UTF8);
 Page variables = new Page("variables", variableText);
 
-string loopText = File.ReadAllText(@"pages\loops.txt", Encoding.UTF8);
+string loopText = File.ReadAllText(files[1], Encoding.UTF8);
 Page loops = new Page("loops", loopText);
 
-string classsesText = File.ReadAllText(@"pages\classes.txt", Encoding.UTF8);
+string classsesText = File.ReadAllText(files[0], Encoding.UTF8);
 Page classes = new Page("classes", classsesText);
 
 pages.Add(variables);
