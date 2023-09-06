39733508c85b616b32fee50a9f5f18c3131cbeb7
Mads Kristian Willadsen <mads482t@mail.hansenberg.dk>
2023-09-06 14:39:51 +0200

    Le done
    den færdig for nu

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ Book lebook = new Book("The Book Of Goon", pages);
 string[] files = Directory.GetFiles("pages");
 
 List<string> names = lebook.LoadPageNames(files);
+
 lebook.Addpagesnames(files, names);
 
 lebook.Start();
